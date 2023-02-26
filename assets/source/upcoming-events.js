a59f398a0005fab699df494228c1b9b69c70528a
Mariano <devsiderio@tutanota.com>
2023-02-26 20:37:20 -0300

    styles changed and added sorting filter based on event's date

--- assets/source/upcoming-events.js
+++ assets/source/upcoming-events.js
@@ -6,26 +6,7 @@ function createCard(data) {
   let card = "";
   for (const event of dataEvents) {
     const dataEventsDate = event.date;
-    if (dataEventsDate <= currentDate) {
-      card += `
-      <div class="col">
-        <div class="card mb-2 rounded-3 shadow-sm">
-          <div class="card-header py-3 text-bg-info">
-            <h6 class="my-0 fw-normal">Past Event</h4>
-          </div>
-          <div class="card-body">
-          <img src="${event.image}" class="card-img-top rounded-3" alt="${event.name}">
-            <h1 class="card-title pricing-card-title my-1">${event.name}</h1>
-            <ul class="list-unstyled mt-2 mb-2">
-              <li>${event.description}</li>
-              <hr>
-              <li>Price: $${event.price}</li>
-            </ul>
-            <button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="window.location.href='./details-1.html';">See more</button>
-          </div>
-        </div>
-      </div>`;
-    } else {
+    if (dataEventsDate >= currentDate) {
       card += `
       <div class="col">
         <div class="card mb-2 rounded-3 shadow-sm">
